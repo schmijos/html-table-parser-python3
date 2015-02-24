8202b6172283041596aee1b18ba30408191419b6
Josua Schmid <josua.schmid@renuo.ch>
2015-02-24 20:57:46 +0100

    pep8 checked

--- example_of_usage.py
+++ example_of_usage.py
@@ -1,19 +1,22 @@
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Created:     04.03.2014
 # Copyright:   (c) Josua Schmid 2014
 # Licence:     GPLv3
 #
 # Sample script for parsing HTML tables
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 import urllib.request
 from pprint import pprint
 from html_table_parser import HTMLTableParser
+
 
 def url_get_contents(url):
     """ Opens a website and read its binary contents (HTTP Response Body) """
     req = urllib.request.Request(url=url)
     f = urllib.request.urlopen(req)
     return f.read()
+
 
 def main():
     url = 'http://www.twitter.com'
@@ -22,6 +25,7 @@ def main():
     p = HTMLTableParser()
     p.feed(xhtml)
     pprint(p.tables)
+
 
 if __name__ == '__main__':
     main()
