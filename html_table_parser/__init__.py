e44e6a80195835f23a2d2bda59b896c6f9b18a1c
Josua Schmid <josua.schmid@renuo.ch>
2015-02-24 20:46:31 +0100

    pep8 checked

--- html_table_parser/__init__.py
+++ html_table_parser/__init__.py
@@ -1,9 +1,5 @@
-""" Simple class for parsing an (x)html string to extract tables.
-Written in python3
-"""
+from .parser import HTMLTableParser
 
 __author__ = 'Josua Schmid'
 __version__ = '0.1.0'
 __licence__ = 'GPLv3'
-
-from .parser import HTMLTableParser
