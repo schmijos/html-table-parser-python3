2f676061c1ab65e5f276ab91af77d5eebcdbdfaa
David McGinnis <656337+davidov541@users.noreply.github.com>
2021-03-05 12:02:12 -0600

    Adding test to make sure we handle multiple tables correctly.

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,4 +1,5 @@
 from os import path
+from typing import List
 import unittest
 
 from html_table_parser import HTMLTableParser
@@ -9,20 +10,34 @@ class ParserTest(unittest.TestCase):
             return f.read()
         self.assertTrue(False, "Invalid input file name")
 
+    def checkTableValues(self, actual: List[List[str]], initial_value: int):
+        self.assertEqual(3, len(actual))
+        self.assertTrue(all([len(a) == 10 for a in actual]))
+        current_cell_value = initial_value
+
+        for r in range(3):
+            for c in range(10):
+                self.assertEqual(current_cell_value, int(actual[r][c]))
+                current_cell_value = current_cell_value + 1
+
     def test_singleTable(self):
         input = self.getTestInput("single_table")
-        print(input)
 
         uut = HTMLTableParser(decode_html_entities=False, data_separator=' ')
         uut.feed(input)
         actual = uut.tables
 
         self.assertEqual(1, len(actual))
-        self.assertEqual(3, len(actual[0]))
-        self.assertTrue(all([len(a) == 10 for a in actual[0]]))
-        current_cell_value = 0
-        
-        for r in range(3):
-            for c in range(10):
-                self.assertEqual(current_cell_value, int(actual[0][r][c]))
-                current_cell_value = current_cell_value + 1
+        self.checkTableValues(actual[0], 0)
+
+    def test_twoTables(self):
+        input = self.getTestInput("two_tables")
+
+        uut = HTMLTableParser(decode_html_entities=False, data_separator=' ')
+        uut.feed(input)
+        actual = uut.tables
+
+        self.assertEqual(2, len(actual))
+
+        self.checkTableValues(actual[0], 0)
+        self.checkTableValues(actual[1], 100)
